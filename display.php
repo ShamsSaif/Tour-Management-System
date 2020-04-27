bda298b834e590aa6063fd79d8d08cb250e3cf1a
lifectusef <31947114+lifectusef@users.noreply.github.com>
2020-04-27 15:17:17 +1000

    Add files via upload

---
+++ display.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<head>
+	<title> All Locations </title>
+</head>
+
+
+<?php
+
+
+require_once('connect.php');
+$query =  'select * from locations';
+$result = mysqli_query($connection,$query);
+
+
+$row_count = mysqli_num_rows($result);
+
+
+for($i=0; $i < $row_count; $i++) {
+$row[] = mysqli_fetch_array($result);
+}
+foreach($row as $next) {
+
+echo '<h1>'.$next['id'].'</h1>';
+echo '<h1>'.$next['locationName'].'</h1>';
+echo '<p>'.$next['description'].'</p>';
+
+}
+
+mysqli_close($connection);
+?>
