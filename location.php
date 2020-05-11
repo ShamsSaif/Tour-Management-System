208e22ef07eb8ed4f0e9b07b4103f36ce6c376aa
lifectusef <31947114+lifectusef@users.noreply.github.com>
2020-05-11 17:08:44 +1000

    Update location.php

--- location.php
+++ location.php
@@ -8,9 +8,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="style.css" rel="stylesheet"  type="text/css"/>
 
-</head>
-
-
 <!DOCTYPE html>
 <html lang = "EN">
 
