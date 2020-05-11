d97c3852dd7a87f9a74eee0db20066b2bcf580f2
lifectusef <31947114+lifectusef@users.noreply.github.com>
2020-05-11 18:35:18 +1000

    Update and rename delete.php to delete_tour.php

---
+++ delete_tour.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+	<title> Confirm Delete </title>
+</head>
+
+<?php
+
+	require_once('connect.php');
+	
+	$id = $_POST['id'];
+	$query = "select * from tours where tourName = $tourName";
+	$result = mysqli_query($connection, $query);
+	$row = mysqli_fetch_array($result);
+	
+	echo "<div id='container'>";
+	echo "<section class='data'>";
+	echo '<h3> Are you sure you want to delete this record?</h3>';
+	
+	
+	echo '<p>ID: ' . $row['id'] . '<p>';
+	echo '<p>Tour Name: ' . $row['tourName'] . '<p>';
+	echo '<p>Description: ' . $row['Description'] . '<p>';
+
+	
+	
+	echo '<p>';
+	echo "<a href='delete_confirm.php'>Cancel</a>";
+	echo "<a href='delete.php?tourName={$row['tourName']}'>Delete</a>";
+	echo '</p>';
+	
+echo '</section>';
+
+?>
